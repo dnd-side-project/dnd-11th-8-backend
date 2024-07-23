ecb09fc0a1bf4dc540c7c13ddcf47f3f1c8895c4
Dompoo <dlckdrms0517@gmail.com>
2024-07-24 00:41:20 +0900

    refactor: 구현부 삭제

--- src/main/kotlin/dnd11th/blooming/api/service/PlantService.kt
+++ src/main/kotlin/dnd11th/blooming/api/service/PlantService.kt
@@ -2,16 +2,26 @@ package dnd11th.blooming.api.service
 
 import dnd11th.blooming.api.dto.PlantSaveRequest
 import dnd11th.blooming.api.dto.PlantSaveResponse
-import dnd11th.blooming.service.implement.PlantWriter
+import dnd11th.blooming.domain.entity.Plant
+import dnd11th.blooming.domain.repository.PlantRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
 class PlantService(
-    private val plantWriter: PlantWriter,
+    private val plantRepository: PlantRepository,
 ) {
     fun savePlant(request: PlantSaveRequest): PlantSaveResponse {
-        return plantWriter.saveOne(request)
+        val plant =
+            Plant(
+                scientificName = request.scientificName,
+                name = request.name,
+                startDate = request.startDate,
+                lastWateredDate = request.lastWateredDate,
+                waterAlarm = request.waterAlarm,
+                nutrientsAlarm = request.nutrientsAlarm,
+            )
+        return PlantSaveResponse.from(plantRepository.save(plant))
     }
 }
--- src/test/kotlin/dnd11th/blooming/service/PlantServiceTest.kt
+++ src/test/kotlin/dnd11th/blooming/service/PlantServiceTest.kt
@@ -1,9 +1,9 @@
 package dnd11th.blooming.service
 
 import dnd11th.blooming.api.dto.PlantSaveRequest
-import dnd11th.blooming.api.dto.PlantSaveResponse
 import dnd11th.blooming.api.service.PlantService
-import dnd11th.blooming.service.implement.PlantWriter
+import dnd11th.blooming.domain.entity.Plant
+import dnd11th.blooming.domain.repository.PlantRepository
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -11,14 +11,19 @@ import io.mockk.mockk
 import java.time.LocalDate
 
 class PlantServiceTest : BehaviorSpec({
-    val plantWriter = mockk<PlantWriter>()
-    val plantService = PlantService(plantWriter)
+    val plantRepsitory = mockk<PlantRepository>()
+    val plantService = PlantService(plantRepsitory)
 
     Context("식물 저장") {
         beforeTest {
-            every { plantWriter.saveOne(any()) } returns
-                PlantSaveResponse(
-                    id = ID,
+            every { plantRepsitory.save(any()) } returns
+                Plant(
+                    scientificName = SCIENTIFIC_NAME,
+                    name = NAME,
+                    startDate = START_DATE,
+                    lastWateredDate = LAST_WATERED_DATE,
+                    waterAlarm = true,
+                    nutrientsAlarm = false,
                 )
         }
         Given("정상 요청이 왔을 때") {
@@ -34,14 +39,13 @@ class PlantServiceTest : BehaviorSpec({
             When("식물을 저장하면") {
                 val response = plantService.savePlant(request)
                 Then("정상적으로 저장되어야 한다.") {
-                    response.id shouldBe 1
+                    response.id shouldBe 0
                 }
             }
         }
     }
 }) {
     companion object {
-        const val ID = 1L
         const val SCIENTIFIC_NAME = "몬스테라 델리오사"
         const val NAME = "뿡뿡이"
         val START_DATE: LocalDate = LocalDate.of(2024, 4, 19)
