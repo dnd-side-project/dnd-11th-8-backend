c5b522c66cd00850a87632ba2fe36c01a0195ebd
Dompoo <dlckdrms0517@gmail.com>
2024-07-25 02:31:59 +0900

    refactor: 사용중인 예외에 ErrorType 적용

--- src/main/kotlin/dnd11th/blooming/common/exception/InvalidDateException.kt
+++ src/main/kotlin/dnd11th/blooming/common/exception/InvalidDateException.kt
@@ -1,12 +1,3 @@
 package dnd11th.blooming.common.exception
 
-import org.springframework.http.HttpStatus
-
-class InvalidDateException(
-    message: String = "올바르지 않은 날짜입니다.",
-) : MyException(statusCode, code, message) {
-    companion object {
-        val statusCode = HttpStatus.BAD_REQUEST
-        val code = ExceptionCode.INVALID_DATE
-    }
-}
+class InvalidDateException(errorType: ErrorType) : MyException(errorType)
