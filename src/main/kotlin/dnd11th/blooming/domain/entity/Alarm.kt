f071be5edf91a4028dadc46e0dda026188812f78
Dompoo <dlckdrms0517@gmail.com>
2024-07-26 22:04:12 +0900

    refactor: Alarm 엔티티를 @Embeddable로 수정하고, Plant 엔티티에 Embed

--- src/main/kotlin/dnd11th/blooming/domain/entity/Alarm.kt
+++ src/main/kotlin/dnd11th/blooming/domain/entity/Alarm.kt
@@ -1,50 +1,20 @@
 package dnd11th.blooming.domain.entity
 
-import dnd11th.blooming.api.dto.PlantSaveRequest
 import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.Embeddable
 
-@Entity
+@Embeddable
 class Alarm(
     @Column(nullable = false)
     var waterAlarm: Boolean,
-    @Column
+    @Column(nullable = false)
     var waterPeriod: Int,
     @Column(nullable = false)
     var nutrientsAlarm: Boolean,
-    @Column
+    @Column(nullable = false)
     var nutrientsPeriod: Int,
     @Column(nullable = false)
     var repotAlarm: Boolean,
-    @Column
+    @Column(nullable = false)
     var repotPeriod: Int,
-) {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long = 0
-
-    companion object {
-        fun from(request: PlantSaveRequest): Alarm =
-            Alarm(
-                waterAlarm = request.waterAlarm ?: false,
-                waterPeriod = request.waterPeriod ?: 3,
-                nutrientsAlarm = request.nutrientsAlarm ?: false,
-                nutrientsPeriod = request.nutrientsPeriod ?: 30,
-                repotAlarm = request.repotAlarm ?: false,
-                repotPeriod = request.repotPeriod ?: 60,
-            )
-
-        fun defaultAlarm(): Alarm =
-            Alarm(
-                waterAlarm = false,
-                waterPeriod = 3,
-                nutrientsAlarm = false,
-                nutrientsPeriod = 30,
-                repotAlarm = false,
-                repotPeriod = 60,
-            )
-    }
-}
+)
--- src/main/kotlin/dnd11th/blooming/domain/entity/Plant.kt
+++ src/main/kotlin/dnd11th/blooming/domain/entity/Plant.kt
@@ -1,13 +1,11 @@
 package dnd11th.blooming.domain.entity
 
-import dnd11th.blooming.api.dto.PlantSaveRequest
 import jakarta.persistence.Column
+import jakarta.persistence.Embedded
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.OneToOne
 import java.time.LocalDate
 
 @Entity
@@ -20,28 +18,10 @@ class Plant(
     var startDate: LocalDate = LocalDate.now(),
     @Column
     var lastWateredDate: LocalDate = LocalDate.now(),
+    @Embedded
+    var alarm: Alarm,
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0
-
-    @OneToOne(fetch = FetchType.LAZY)
-    var alarm: Alarm? = null
-
-    fun setAlarmMapping(alarm: Alarm) {
-        this.alarm = alarm
-    }
-
-    companion object {
-        fun from(
-            request: PlantSaveRequest,
-            alarm: Alarm = Alarm.defaultAlarm(),
-        ): Plant =
-            Plant(
-                scientificName = request.scientificName,
-                name = request.name,
-                startDate = request.startDate,
-                lastWateredDate = request.lastWateredDate,
-            ).apply { setAlarmMapping(alarm) }
-    }
 }
