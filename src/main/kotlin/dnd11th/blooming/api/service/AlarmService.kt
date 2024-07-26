0af17fb78982a69e1c672db41629268147eff89f
Dompoo <dlckdrms0517@gmail.com>
2024-07-26 22:03:00 +0900

    fix: Alarm을 Plant에 @Embedded 하기 위해 기존 컨트롤러/서비스/리포지토리 삭제

--- src/main/kotlin/dnd11th/blooming/api/service/AlarmService.kt
+++
@@ -1,6 +0,0 @@
-package dnd11th.blooming.api.service
-
-import org.springframework.stereotype.Service
-
-@Service
-class AlarmService
--- src/main/kotlin/dnd11th/blooming/domain/repository/AlarmRepository.kt
+++
@@ -1,6 +0,0 @@
-package dnd11th.blooming.domain.repository
-
-import dnd11th.blooming.domain.entity.Alarm
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AlarmRepository : JpaRepository<Alarm, Long>
