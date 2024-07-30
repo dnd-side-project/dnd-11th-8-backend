9ae9c1c187bd2785c5b62a20a972e55e22130d93
Dompoo <dlckdrms0517@gmail.com>
2024-07-30 13:42:33 +0900

    feat: User - Location 연관관계 추가

--- src/main/kotlin/dnd11th/blooming/domain/entity/Location.kt
+++ src/main/kotlin/dnd11th/blooming/domain/entity/Location.kt
@@ -2,9 +2,12 @@ package dnd11th.blooming.domain.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 
 @Entity
 class Location(
@@ -14,4 +17,13 @@ class Location(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    var user: User? = null
+
+    fun setUserRelation(user: User) {
+        this.user = user
+        user.locations.add(this)
+    }
 }
--- src/main/kotlin/dnd11th/blooming/domain/entity/User.kt
+++ src/main/kotlin/dnd11th/blooming/domain/entity/User.kt
@@ -1,10 +1,12 @@
 package dnd11th.blooming.domain.entity
 
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 
 @Entity
@@ -17,6 +19,9 @@ class User(
     @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
+
+    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL])
+    val locations: MutableList<Location> = mutableListOf()
 
     val email: String = email
 
