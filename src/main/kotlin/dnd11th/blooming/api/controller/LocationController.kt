f75ed75cf1d6d2b6e01d7c55b86fbd9ab98d155c
Dompoo <dlckdrms0517@gmail.com>
2024-07-31 13:26:34 +0900

    refactor: 위치관련 API들의 URL 주소를 RequestMapping으로 공통화

--- src/main/kotlin/dnd11th/blooming/api/controller/LocationController.kt
+++ src/main/kotlin/dnd11th/blooming/api/controller/LocationController.kt
@@ -11,27 +11,29 @@ import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/location")
 class LocationController(
     private val locationService: LocationService,
 ) {
-    @PostMapping("/location")
+    @PostMapping
     fun saveLocation(
         @RequestBody request: LocationSaveRequest,
     ): LocationSaveResponse = locationService.saveLocation(request)
 
-    @GetMapping("/location")
+    @GetMapping
     fun findAllLocation(): List<LocationResponse> = locationService.findAllLocation()
 
-    @PatchMapping("/location/{locationId}")
+    @PatchMapping("/{locationId}")
     fun modifyLocation(
         @PathVariable locationId: Long,
         @RequestBody request: LocationModifyRequest,
     ): LocationResponse = locationService.modifyLocation(locationId, request)
 
-    @DeleteMapping("/location/{locationId}")
+    @DeleteMapping("/{locationId}")
     fun deleteLocation(
         @PathVariable locationId: Long,
     ) = locationService.deleteLocation(locationId)
