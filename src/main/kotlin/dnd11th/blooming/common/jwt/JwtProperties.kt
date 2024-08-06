6b218407051a206fcce3b9201310ee820b485806
stophwan <jjw981210@gmail.com>
2024-08-06 23:34:13 +0900

    chore: yml 수정

--- src/main/kotlin/dnd11th/blooming/common/jwt/JwtProperties.kt
+++ src/main/kotlin/dnd11th/blooming/common/jwt/JwtProperties.kt
@@ -2,7 +2,7 @@ package dnd11th.blooming.common.jwt
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(value = "auth.jwt")
+@ConfigurationProperties(prefix = "auth.jwt")
 data class JwtProperties(
     val access: TokenProperties,
     val refresh: TokenProperties,
