4c77304dd441354cb34a2067c36f653f535e6d7a
stophwan <jjw981210@gmail.com>
2024-08-06 21:10:54 +0900

    chore: add swagger dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ allOpen {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
