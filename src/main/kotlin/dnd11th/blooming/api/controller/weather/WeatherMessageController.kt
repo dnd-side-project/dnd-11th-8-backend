82175b794ef0db9f5c91341d67b512d0e05ef8f2
stophwan <jjw981210@gmail.com>
2024-08-11 13:30:58 +0900

    feat: 당일 날씨 메시지 반환 로직 구현

---
+++ src/main/kotlin/dnd11th/blooming/api/controller/weather/WeatherMessageController.kt
@@ -0,0 +1,23 @@
+package dnd11th.blooming.api.controller.weather
+
+import dnd11th.blooming.api.dto.weather.WeatherMessageResponse
+import dnd11th.blooming.api.service.weather.WeatherMessage
+import dnd11th.blooming.api.service.weather.WeatherService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+
+@RestController
+@RequestMapping("/api/v1/weather-message")
+class WeatherMessageController(
+    private val weatherService: WeatherService
+) {
+
+    @GetMapping
+    fun getWeatherMessage() : List<WeatherMessageResponse> {
+        val weatherMessages: List<WeatherMessage> =
+            weatherService.createPlantMessageByWeather(LocalDateTime.now())
+        return weatherMessages.map { weatherMessage -> WeatherMessageResponse.from(weatherMessage) }
+    }
+}
