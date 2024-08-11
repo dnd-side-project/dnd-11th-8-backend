03006ecc02e374f72e03cf8d5c076ac2e0c68466
stophwan <jjw981210@gmail.com>
2024-08-11 15:00:08 +0900

    refactor: WeatherMessage message를 mutableList -> List 변경

--- src/main/kotlin/dnd11th/blooming/api/service/weather/WeatherMessage.kt
+++ src/main/kotlin/dnd11th/blooming/api/service/weather/WeatherMessage.kt
@@ -1,8 +1,8 @@
 package dnd11th.blooming.api.service.weather
 
 enum class WeatherMessage(val title: String, val message: List<String>) {
-    HUMIDITY("과습주의보", mutableListOf("과습이에요", "과습입니다.")),
-    DRY("건조주의보", mutableListOf("건조에요", "건조입니다")),
-    COLD("한파주의보", mutableListOf("한파입니다", "한파에요")),
-    HOT("더위주의보", mutableListOf("덥습니다", "더워요")),
+    HUMIDITY("과습주의보", listOf("과습이에요", "과습입니다.")),
+    DRY("건조주의보", listOf("건조에요", "건조입니다")),
+    COLD("한파주의보", listOf("한파입니다", "한파에요")),
+    HOT("더위주의보", listOf("덥습니다", "더워요")),
 }
