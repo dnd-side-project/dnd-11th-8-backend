437c425eb06a72dc7ffa7e1355499a32eece0fb9
Dompoo <dlckdrms0517@gmail.com>
2024-08-11 20:30:56 +0900

    feat: 이미지 엔티티, 리포지토리 개발

---
+++ src/main/kotlin/dnd11th/blooming/domain/entity/Image.kt
@@ -0,0 +1,26 @@
+package dnd11th.blooming.domain.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+@Entity
+class Image(
+    @Column
+    var url: String,
+    @Column
+    var favorite: Boolean,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "myplant_id")
+    var myPlant: MyPlant? = null
+}
