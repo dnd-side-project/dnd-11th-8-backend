14025435f2e46f27017374d50db503c69e10478a
Dompoo <dlckdrms0517@gmail.com>
2024-08-11 10:45:04 +0900

    chore: MessageFactory의 변하지 않는 메세지들을 정적으로 수정

--- src/main/kotlin/dnd11th/blooming/api/service/myplant/MyPlantMessageFactory.kt
+++ src/main/kotlin/dnd11th/blooming/api/service/myplant/MyPlantMessageFactory.kt
@@ -6,18 +6,20 @@ import java.time.Period
 
 @Component
 class MyPlantMessageFactory {
-    private val wateredTitle = "마지막으로 물 준 날"
-    private val fertilizerTitle = "비료주기"
-    private val greetingMessageList =
-        listOf(
-            "%s님 좋아요!\n초보식집사로서 멋진 데뷔네요.",
-        )
+    companion object {
+        private const val WATER_TITLE = "마지막으로 물 준 날"
+        private const val FERTILIZER_TITLE = "비료주기"
+        private val greetingMessageList =
+            listOf(
+                "%s님 좋아요!\n초보식집사로서 멋진 데뷔네요.",
+            )
+    }
 
     fun createGreetingMessage(username: String): String {
         return greetingMessageList.random().format(username)
     }
 
-    fun createWateredTitle(): String = wateredTitle
+    fun createWateredTitle(): String = WATER_TITLE
 
     fun createWateredInfo(
         wateredDate: LocalDate,
@@ -32,7 +34,7 @@ class MyPlantMessageFactory {
         return "${wateredDate}\n${daysPeriod}일전"
     }
 
-    fun createFertilizerTitle(): String = fertilizerTitle
+    fun createFertilizerTitle(): String = FERTILIZER_TITLE
 
     fun createFertilizerInfo(
         fertilizerDate: LocalDate,
