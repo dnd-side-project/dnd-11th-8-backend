64a60ca0d3abf7381f6fedc3f2411bc4df972b4e
Dompoo <dlckdrms0517@gmail.com>
2024-08-12 17:44:39 +0900

    fix: 내 식물 저장시에 별명값을 필수로 받도록 수정,내 식물 수정시에 별명값을 받지 않아도 되도록 수정

--- src/main/kotlin/dnd11th/blooming/api/dto/myplant/MyPlantModifyRequest.kt
+++ src/main/kotlin/dnd11th/blooming/api/dto/myplant/MyPlantModifyRequest.kt
@@ -1,11 +1,9 @@
 package dnd11th.blooming.api.dto.myplant
 
-import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.PastOrPresent
 import java.time.LocalDate
 
 data class MyPlantModifyRequest(
-    @field:NotBlank(message = "식물 별명은 비어있을 수 없습니다.")
     val nickname: String?,
     val locationId: Long?,
     @field:PastOrPresent(message = "키우기 시작한 날짜는 미래일 수 없습니다.")
