a9e1d72b6b30d5790b6d6bd0427fb7fe371de865
stophwan <jjw981210@gmail.com>
2024-08-15 05:50:31 +0900

    test: update jwtprovider test

--- src/test/kotlin/dnd11th/blooming/common/jwt/JwtProviderTest.kt
+++ src/test/kotlin/dnd11th/blooming/common/jwt/JwtProviderTest.kt
@@ -12,7 +12,6 @@ class JwtProviderTest : DescribeSpec({
 
     val userId = 1L
     val email = "blooming@naver.com"
-    val nickname = "블루밍"
     val accessSecret = "coffeecoffeecoffeecoffeecoffeecoffeecoffeevcoffeecoffeecoffeecoffeecoffeecoffee"
     val refreshSecret = "coffeecoffeecoffeecoffeecoffeecoffeecoffeevcoffeecoffeecoffeecoffeecoffeecoffee"
     val jwtProperties =
@@ -30,15 +29,13 @@ class JwtProviderTest : DescribeSpec({
             val secretKey: SecretKey = Keys.hmacShaKeyFor(accessSecret.toByteArray())
 
             it("토큰을 생성한다") {
-                val token = jwtProvider.generateAccessToken(userId, email, nickname)
+                val token = jwtProvider.generateAccessToken(userId, email)
                 val claims: Claims = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).payload
                 val claimId = claims["id"]
                 val claimEmail = claims["email"]
-                val claimNickname = claims["nickname"]
 
                 claimId shouldBe userId
                 claimEmail shouldBe email
-                claimNickname shouldBe nickname
             }
         }
     }
@@ -46,16 +43,14 @@ class JwtProviderTest : DescribeSpec({
     describe("verifyToken") {
 
         context("userId, email이 전달되면") {
-            val token = jwtProvider.generateAccessToken(userId, email, nickname)
+            val token = jwtProvider.generateAccessToken(userId, email)
             it("토큰을 생성한다") {
                 val userClaims: UserClaims = jwtProvider.resolveAccessToken(token)
                 val claimId = userClaims.id
                 val claimEmail = userClaims.email
-                val claimNickname = userClaims.nickname
 
                 claimId shouldBe userId
                 claimEmail shouldBe email
-                claimNickname shouldBe nickname
             }
         }
     }
