1456ee8e20c45fcc574739f1768f1000cf8ce747
stophwan <jjw981210@gmail.com>
2024-08-17 14:43:38 +0900

    feat: AppleClient 생성

--- src/main/kotlin/dnd11th/blooming/client/oauth/AppleOauthClient.kt
+++ src/main/kotlin/dnd11th/blooming/client/oauth/AppleOauthClient.kt
@@ -1,14 +1,14 @@
-package dnd11th.blooming.client.kakao
+package dnd11th.blooming.client.oauth
 
 import dnd11th.blooming.client.dto.OidcPublicKeys
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 
 @FeignClient(
-    name = "KakaoOauthClient",
-    url = "https://kauth.kakao.com",
+    name = "AppleOauthClient",
+    url = "https://appleid.apple.com",
 )
-interface KakaoOauthClient {
-    @GetMapping("/.well-known/jwks.json")
+interface AppleOauthClient {
+    @GetMapping("/auth/keys")
     fun getPublicKeys(): OidcPublicKeys
 }
