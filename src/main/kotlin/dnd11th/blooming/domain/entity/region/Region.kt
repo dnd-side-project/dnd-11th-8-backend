daa46a285a2690b645fb429791e30e02feedc781
stophwan <jjw981210@gmail.com>
2024-08-18 08:18:49 +0900

    refactor: CityProvince & SiGunGu 엔티티 삭제

--- src/main/kotlin/dnd11th/blooming/domain/entity/region/Region.kt
+++ src/main/kotlin/dnd11th/blooming/domain/entity/region/Region.kt
@@ -2,13 +2,9 @@ package dnd11th.blooming.domain.entity.region
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.OneToOne
 
 @Entity
 class Region(
@@ -16,13 +12,9 @@ class Region(
     val nx: Int,
     @Column
     val ny: Int,
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "ctp_id")
-    val cityProvince: CityProvince? = null,
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "sig_id")
-    val siGunGu: SiGunGu? = null,
+    @Column
+    val name: String,
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
+    val id: Int,
 )
