a67da0ca183f9b85d3edb97821cf9aef7905c8bd
stophwan <jjw981210@gmail.com>
2024-08-17 14:55:12 +0900

    feat: OpenIdTokenResolverSelector 구현

---
+++ src/main/kotlin/dnd11th/blooming/api/service/user/oauth/OpenIdTokenResolverSelector.kt
@@ -0,0 +1,17 @@
+package dnd11th.blooming.api.service.user.oauth
+
+import dnd11th.blooming.domain.entity.user.OauthProvider
+import org.springframework.context.ApplicationContext
+import org.springframework.stereotype.Component
+
+@Component
+class OpenIdTokenResolverSelector(
+    private val applicationContext: ApplicationContext,
+) {
+    fun select(oauthProvider: OauthProvider): OpenIdTokenResolver {
+        return when (oauthProvider) {
+            OauthProvider.KAKAO -> applicationContext.getBean(KakaoIdTokenResolver::class.java)
+            OauthProvider.APPLE -> applicationContext.getBean(AppleIdTokenResolver::class.java)
+        }
+    }
+}
