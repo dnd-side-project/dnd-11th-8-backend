bdf5c909ce4837ba4d51b784af2537b465f91159
Dompoo <dlckdrms0517@gmail.com>
2024-08-20 03:35:26 +0900

    fix: 전체 식물 조회에서 위치가 있는지 없는지 추가 응답

--- src/main/kotlin/dnd11th/blooming/api/dto/myplant/MyPlantResponse.kt
+++ src/main/kotlin/dnd11th/blooming/api/dto/myplant/MyPlantResponse.kt
@@ -13,6 +13,8 @@ data class MyPlantResponse(
     val myPlantId: Long?,
     @field:Schema(description = "내 식물 별명", example = "쫑쫑이")
     val nickname: String,
+    @field:Schema(description = "위치 존재 여부", example = "1")
+    val haveLocation: Boolean,
     @field:Schema(description = "이미지 URL", example = "image.com/7")
     val imageUrl: String,
     @field:Schema(description = "내 식물 학명", example = "몬스테라 델리오사")
@@ -33,6 +35,7 @@ data class MyPlantResponse(
             MyPlantResponse(
                 myPlantId = myPlant.id,
                 nickname = myPlant.nickname,
+                haveLocation = myPlant.location != null,
                 imageUrl = imageUrl,
                 scientificName = myPlant.scientificName,
                 dateSinceLastWater = myPlant.getDateSinceLastWater(now),
