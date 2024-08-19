9cb5c05ce4952b76cb7a08aa218bc82ceab8fa14
Dompoo <dlckdrms0517@gmail.com>
2024-08-19 14:43:42 +0900

    fix: 성장온도 값을 String에서 Int로 변경

--- src/main/kotlin/dnd11th/blooming/api/service/guide/provider/StaticPlantSimpleMessageProvider.kt
+++ src/main/kotlin/dnd11th/blooming/api/service/guide/provider/StaticPlantSimpleMessageProvider.kt
@@ -127,7 +127,7 @@ class StaticPlantSimpleMessageProvider {
     }
 
     private fun makeSimpleTemperatureDescription(plant: Plant): String {
-        val growTemperatureDescription = "${plant.growTemperature.displayName}사이"
+        val growTemperatureDescription = "${plant.growTemperature.lowTemperature}~${plant.growTemperature.hightTemperature}사이"
         val lowestTemperature = "${plant.lowestTemperature.displayName} 이하는 주의할 것"
 
         return "${growTemperatureDescription}\n$lowestTemperature"
--- src/main/kotlin/dnd11th/blooming/domain/entity/plant/GrowTemperature.kt
+++ src/main/kotlin/dnd11th/blooming/domain/entity/plant/GrowTemperature.kt
@@ -1,8 +1,15 @@
 package dnd11th.blooming.domain.entity.plant
 
 enum class GrowTemperature(
-    val displayName: String,
+    val lowTemperature: Int,
+    val hightTemperature: Int,
 ) {
-    GROW_TEMPERATURE_16_20("16~20도"),
-    GROW_TEMPERATURE_21_25("21~25도"),
+    GROW_TEMPERATURE_16_20(
+        16,
+        20,
+    ),
+    GROW_TEMPERATURE_21_25(
+        16,
+        20,
+    ),
 }
