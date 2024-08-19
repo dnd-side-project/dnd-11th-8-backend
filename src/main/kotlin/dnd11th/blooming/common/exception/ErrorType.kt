cf32619ec99ebbf72217249c39fb93adc11e1f43
Dompoo <dlckdrms0517@gmail.com>
2024-08-20 02:25:49 +0900

    chore: 불필요한 ErrorType 제거

--- src/main/kotlin/dnd11th/blooming/common/exception/ErrorType.kt
+++ src/main/kotlin/dnd11th/blooming/common/exception/ErrorType.kt
@@ -7,9 +7,6 @@ enum class ErrorType(val status: HttpStatus, var message: String, val logLevel: 
     // COMMON
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error has occurred.", LogLevel.ERROR),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "요청 파라미터가 올바르지 않습니다.", LogLevel.DEBUG),
-    CREATE_NOT_AVAILABLE(HttpStatus.BAD_REQUEST, "추가할 수 없습니다.", LogLevel.DEBUG),
-    MODIFY_NOT_AVAILABLE(HttpStatus.BAD_REQUEST, "수정할 수 없습니다.", LogLevel.DEBUG),
-    DELETE_NOT_AVAILABLE(HttpStatus.BAD_REQUEST, "삭제할 수 없습니다.", LogLevel.DEBUG),
 
     // MyPlant
     NOT_FOUND_MYPLANT(HttpStatus.NOT_FOUND, "존재하지 않는 내 식물입니다.", LogLevel.DEBUG),
