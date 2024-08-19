d4c2b19a7ae56c3db5a05eb5412487f0a52ce7db
Dompoo <dlckdrms0517@gmail.com>
2024-08-19 15:21:48 +0900

    chore: 습도에서 필요없는 필드 제거, Illust 엔티티 추가

--- src/main/kotlin/dnd11th/blooming/domain/entity/plant/Humidity.kt
+++ src/main/kotlin/dnd11th/blooming/domain/entity/plant/Humidity.kt
@@ -3,21 +3,17 @@ package dnd11th.blooming.domain.entity.plant
 enum class Humidity(
     val displayName: String,
     val humidityLevel: String,
-    val apiName: String,
 ) {
     HUMIDITY_0_40(
         "40% 이하",
         "낮은 습도",
-        "40% 이하",
     ),
     HUMIDITY_40_70(
         "40%~70%",
         "중간 습도",
-        "40% ~ 70%",
     ),
     HUMIDITY_70_100(
         "70% 이상",
         "높은 습도",
-        "70% 이상",
     ),
 }
