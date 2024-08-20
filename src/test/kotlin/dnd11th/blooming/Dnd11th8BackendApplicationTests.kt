a75dfafd18b3240399c2c3b161c321f00e84d50e
stophwan <jjw981210@gmail.com>
2024-08-21 03:27:51 +0900

    fix: mock fcm config

--- src/test/kotlin/dnd11th/blooming/Dnd11th8BackendApplicationTests.kt
+++ src/test/kotlin/dnd11th/blooming/Dnd11th8BackendApplicationTests.kt
@@ -1,12 +1,24 @@
 package dnd11th.blooming
 
+import dnd11th.blooming.client.config.FcmConfig
+import io.mockk.mockk
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
 @SpringBootTest
 class Dnd11th8BackendApplicationTests {
+    @TestConfiguration
+    class TestConfig {
+        @Bean
+        fun fcmConfig(): FcmConfig {
+            return mockk(relaxed = true)
+        }
+    }
+
     @Test
     fun contextLoads() {
     }
--- src/test/kotlin/dnd11th/blooming/api/service/myplant/MyPlantServiceIntegrationTest.kt
+++ src/test/kotlin/dnd11th/blooming/api/service/myplant/MyPlantServiceIntegrationTest.kt
@@ -1,6 +1,7 @@
 package dnd11th.blooming.api.service.myplant
 
 import dnd11th.blooming.api.dto.myplant.MyPlantQueryCreteria
+import dnd11th.blooming.client.config.FcmConfig
 import dnd11th.blooming.domain.entity.Alarm
 import dnd11th.blooming.domain.entity.Image
 import dnd11th.blooming.domain.entity.Location
@@ -10,8 +11,11 @@ import dnd11th.blooming.domain.repository.LocationRepository
 import dnd11th.blooming.domain.repository.MyPlantRepository
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.mockk
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
 import org.springframework.test.context.ActiveProfiles
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -30,6 +34,14 @@ class MyPlantServiceIntegrationTest : DescribeSpec() {
 
     @Autowired
     lateinit var locationRepository: LocationRepository
+
+    @TestConfiguration
+    class TestConfig {
+        @Bean
+        fun fcmConfig(): FcmConfig {
+            return mockk(relaxed = true)
+        }
+    }
 
     init {
         afterTest {
