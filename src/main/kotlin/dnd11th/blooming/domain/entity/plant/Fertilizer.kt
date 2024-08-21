24d7d63d8dbf8d02a4fc230abc477dd222a4009f
Dompoo <dlckdrms0517@gmail.com>
2024-08-22 01:14:23 +0900

    fix: Fertilizer Enum 값 추가

--- src/main/kotlin/dnd11th/blooming/domain/entity/plant/Fertilizer.kt
+++ src/main/kotlin/dnd11th/blooming/domain/entity/plant/Fertilizer.kt
@@ -4,6 +4,10 @@ enum class Fertilizer(
     val periodWeek: Int,
     val apiName: String,
 ) {
+    NOT_DEMAND(
+        12,
+        "비료를 요구하지 않음",
+    ),
     LOW_DEMAND(
         8,
         "비료를 거의 요구하지 않음",
@@ -11,5 +15,9 @@ enum class Fertilizer(
     DEMAND(
         4,
         "비료를 보통 요구함",
+    ),
+    HIGH_DEMAND(
+        4,
+        "비료를 많이 요구함",
     ),
 }
