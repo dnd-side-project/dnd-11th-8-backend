3837370adeef98393471e1c2ec222085d619f3cc
Dompoo <dlckdrms0517@gmail.com>
2024-08-21 23:33:41 +0900

    fix: GrowTemperature Enum 오타 수정

--- src/main/kotlin/dnd11th/blooming/api/service/guide/provider/StaticPlantSimpleMessageProvider.kt
+++ src/main/kotlin/dnd11th/blooming/api/service/guide/provider/StaticPlantSimpleMessageProvider.kt
@@ -129,7 +129,7 @@ class StaticPlantSimpleMessageProvider {
     private fun makeSimpleTemperatureDescription(plant: Plant): String {
         val growTemperatureDescription =
             "${plant.growTemperature.lowTemperature}" +
-                "~${plant.growTemperature.hightTemperature}사이"
+                "~${plant.growTemperature.highTemperature}사이"
         val lowestTemperature = "${plant.lowestTemperature.temperature}도 이하는 주의할 것"
 
         return "${growTemperatureDescription}\n$lowestTemperature"
