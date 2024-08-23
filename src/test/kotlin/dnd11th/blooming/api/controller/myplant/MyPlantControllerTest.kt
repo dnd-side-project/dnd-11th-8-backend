485c8da43e336610b0fa71d1a43fedb39bea7ae8
Dompoo <dlckdrms0517@gmail.com>
2024-08-23 13:35:35 +0900

    feat: 내 식물 상세 조회에서 응답 필드에 LocalDate값 포함

--- src/test/kotlin/dnd11th/blooming/api/controller/myplant/MyPlantControllerTest.kt
+++ src/test/kotlin/dnd11th/blooming/api/controller/myplant/MyPlantControllerTest.kt
@@ -14,6 +14,7 @@ import dnd11th.blooming.support.WebMvcDescribeSpec
 import io.mockk.every
 import io.mockk.just
 import io.mockk.runs
+import org.assertj.core.api.LocalDateAssert
 import org.hamcrest.CoreMatchers.equalTo
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.delete
@@ -41,8 +42,8 @@ class MyPlantControllerTest : WebMvcDescribeSpec() {
                             nickname = NICKNAME,
                             locationId = LOCATION_ID,
                             startDate = START_DATE,
-                            lastWateredDate = LAST_WATERED_DATE,
-                            lastFertilizerDate = LAST_FERTILIZER_DATE,
+                            lastWateredDate = LAST_WATERED_DATE_INT,
+                            lastFertilizerDate = LAST_FERTILIZER_DATE_INT,
                             waterAlarm = WATER_ALARM,
                             waterPeriod = WATER_PERIOD,
                             fertilizerAlarm = FERTILIZER_ALARM,
@@ -155,8 +156,10 @@ class MyPlantControllerTest : WebMvcDescribeSpec() {
                         withDays = WITH_DAYS,
                         lastWateredTitle = LAST_WATERED_TITLE,
                         lastWateredInfo = LAST_WATERED_INFO,
+	                    lastWateredDate = LAST_WATERED_DATE,
                         lastFertilizerTitle = LAST_FERTILIZER_TITLE,
                         lastFertilizerInfo = LAST_FERTILIZER_INFO,
+	                    lastFertilizerDate = LAST_FERTILIZER_DATE,
                         waterAlarm = WATER_ALARM,
                         waterPeriod = WATER_PERIOD,
                         fertilizerAlarm = FERTILIZER_ALARM,
@@ -243,8 +246,8 @@ class MyPlantControllerTest : WebMvcDescribeSpec() {
                             nickname = NICKNAME,
                             locationId = LOCATION_ID,
                             startDate = START_DATE,
-                            lastWateredDate = LAST_WATERED_DATE,
-                            lastFertilizerDate = LAST_FERTILIZER_DATE,
+                            lastWateredDate = LAST_WATERED_DATE_INT,
+                            lastFertilizerDate = LAST_FERTILIZER_DATE_INT,
                         ),
                     )
                 it("정상 흐름이 반환되어야 한다.") {
@@ -263,8 +266,8 @@ class MyPlantControllerTest : WebMvcDescribeSpec() {
                             nickname = NICKNAME,
                             locationId = LOCATION_ID,
                             startDate = START_DATE,
-                            lastWateredDate = LAST_WATERED_DATE,
-                            lastFertilizerDate = LAST_FERTILIZER_DATE,
+                            lastWateredDate = LAST_WATERED_DATE_INT,
+                            lastFertilizerDate = LAST_FERTILIZER_DATE_INT,
                         ),
                     )
                 it("예외응답이 반환되어야 한다.") {
@@ -285,8 +288,8 @@ class MyPlantControllerTest : WebMvcDescribeSpec() {
                             nickname = NICKNAME,
                             locationId = LOCATION_ID + 1,
                             startDate = START_DATE,
-                            lastWateredDate = LAST_WATERED_DATE,
-                            lastFertilizerDate = LAST_FERTILIZER_DATE,
+                            lastWateredDate = LAST_WATERED_DATE_INT,
+                            lastFertilizerDate = LAST_FERTILIZER_DATE_INT,
                         ),
                     )
                 it("예외응답이 반환되어야 한다.") {
@@ -440,8 +443,10 @@ class MyPlantControllerTest : WebMvcDescribeSpec() {
         const val LOCATION_NAME = "거실"
         val START_DATE: LocalDate = LocalDate.of(2024, 4, 19)
         const val WITH_DAYS = 234
-        const val LAST_WATERED_DATE = 1
-        const val LAST_FERTILIZER_DATE = 2
+        const val LAST_WATERED_DATE_INT = 1
+	    val LAST_WATERED_DATE: LocalDate = CURRENT_DAY.minusDays(LAST_WATERED_DATE_INT.toLong())
+	    const val LAST_FERTILIZER_DATE_INT = 2
+	    val LAST_FERTILIZER_DATE: LocalDate = CURRENT_DAY.minusDays(LAST_FERTILIZER_DATE_INT.toLong())
         val LAST_HEALTHCHECK_DATE: LocalDate = LocalDate.of(2024, 6, 15)
         const val DATE_SINCE_LAST_WATER = 3
         const val DATE_SINCE_LAST_FERTILIZER = 3
@@ -460,6 +465,6 @@ class MyPlantControllerTest : WebMvcDescribeSpec() {
         val LAST_FERTILIZER_TITLE = "비료주기"
         val LAST_FERTILIZER_INFO = "이번 달"
         val LAST_WATERED_TITLE = "마지막으로 물 준 날"
-        val LAST_WATERED_INFO = "${LAST_WATERED_DATE}\n1일전"
+        val LAST_WATERED_INFO = "${LAST_WATERED_DATE_INT}\n1일전"
     }
 }
