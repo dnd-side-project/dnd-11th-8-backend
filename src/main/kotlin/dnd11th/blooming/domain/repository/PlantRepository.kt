377e9cb71c9b85aab247670135cc7f62b83d4caa
Dompoo <dlckdrms0517@gmail.com>
2024-09-02 23:01:49 +0900

    feat: HangulUtil을 사용하여 식물 종류 검색하도록 수정

--- src/main/kotlin/dnd11th/blooming/domain/repository/PlantRepository.kt
+++ src/main/kotlin/dnd11th/blooming/domain/repository/PlantRepository.kt
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
 interface PlantRepository : JpaRepository<Plant, Long> {
-    @Query("SELECT p FROM Plant p WHERE p.korName LIKE %:name% OR p.engName LIKE %:name%")
-    fun findAllByNameContaining(
-        @Param("name") name: String,
+    @Query("SELECT p FROM Plant p WHERE p.decomposedkorName LIKE :decomposedKorName")
+    fun findByNameMatchesPattern(
+        @Param("decomposedKorName") decomposedKorName: String,
     ): List<Plant>
 }
