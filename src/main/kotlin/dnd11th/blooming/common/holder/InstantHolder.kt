2ca7d22dd0014569a1db2e27acbe8518cc9ab3b0
정지환 <64758861+stophwan@users.noreply.github.com>
2024-09-05 12:33:51 +0900

    feat: Instant Holder 인터페이스 작성

---
+++ src/main/kotlin/dnd11th/blooming/common/holder/InstantHolder.kt
@@ -0,0 +1,7 @@
+package dnd11th.blooming.common.holder
+
+import java.time.Instant
+
+interface InstantHolder {
+    fun now(): Instant
+}
---
+++ src/main/kotlin/dnd11th/blooming/common/holder/SystemInstantHolder.kt
@@ -0,0 +1,11 @@
+package dnd11th.blooming.common.holder
+
+import org.springframework.stereotype.Component
+import java.time.Instant
+
+@Component
+class SystemInstantHolder : InstantHolder {
+    override fun now(): Instant  {
+        return Instant.now()
+    }
+}
