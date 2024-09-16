a89abe502480ed377c88c8fc383f99750fbad898
stophwan <jjw981210@gmail.com>
2024-09-16 21:23:33 +0900

    refactor: DeviceToken Entity 삭제

--- src/main/kotlin/dnd11th/blooming/domain/repository/devicetoken/DeviceTokenRepository.kt
+++
@@ -1,8 +0,0 @@
-package dnd11th.blooming.domain.repository.devicetoken
-
-import dnd11th.blooming.domain.entity.devicetoken.DeviceToken
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface DeviceTokenRepository : JpaRepository<DeviceToken, Long> {
-    fun findByToken(token: String): DeviceToken?
-}
