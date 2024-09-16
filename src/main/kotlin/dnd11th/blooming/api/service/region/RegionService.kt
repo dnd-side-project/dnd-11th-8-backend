c93ced4d43627ba55759f37b7840431c9f28a511
stophwan <jjw981210@gmail.com>
2024-09-16 15:59:53 +0900

    feat: 지역 검색 FullTextIndex 적용

--- src/main/kotlin/dnd11th/blooming/api/service/region/RegionService.kt
+++ src/main/kotlin/dnd11th/blooming/api/service/region/RegionService.kt
@@ -3,8 +3,6 @@ package dnd11th.blooming.api.service.region
 import dnd11th.blooming.api.dto.region.RegionResponse
 import dnd11th.blooming.domain.entity.region.Region
 import dnd11th.blooming.domain.repository.region.RegionRepository
-import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,13 +10,18 @@ class RegionService(
     private val regionRepository: RegionRepository,
 ) {
     companion object {
-        private const val DEFAULT_PAGE_NUMBER = 0
         private const val DEFAULT_PAGE_SIZE = 30
     }
 
     fun findRegion(name: String): List<RegionResponse> {
-        val pageable: Pageable = PageRequest.of(DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE)
-        val regions: List<Region> = regionRepository.findByNameContaining(name, pageable)
+        val searchQuery = createSearchQuery(name)
+        val regions: List<Region> = regionRepository.findByNameContaining2(searchQuery, DEFAULT_PAGE_SIZE)
         return regions.stream().map { region -> RegionResponse.from(region) }.toList()
+    }
+
+    fun createSearchQuery(input: String): String {
+        return input.split(" ")
+            .filter { it.isNotBlank() }
+            .joinToString(" ") { "+$it" }
     }
 }
