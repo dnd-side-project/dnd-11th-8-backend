9d328d01fb64bf6f322ccc7b28dfa42da848406d
Dompoo <dlckdrms0517@gmail.com>
2024-09-16 23:51:01 +0900

    feat: HttpMessageNotReadableException 예외 핸들링 추가, 로깅 분리

--- api/src/main/kotlin/dnd11th/blooming/api/controller/exception/GlobalExceptionHandler.kt
+++ api/src/main/kotlin/dnd11th/blooming/api/controller/exception/GlobalExceptionHandler.kt
@@ -7,6 +7,7 @@ import dnd11th.blooming.common.exception.MyException
 import dnd11th.blooming.common.util.Logger.Companion.log
 import org.springframework.boot.logging.LogLevel
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -17,17 +18,9 @@ class GlobalExceptionHandler {
     @ExceptionHandler(MyException::class)
     fun handleMyException(exception: MyException): ResponseEntity<ErrorResponse> {
         val errorType = exception.errorType
-        when (errorType.logLevel) {
-            LogLevel.ERROR -> {
-                log.error { "Blooming Exception: ${errorType.message}, Exception: $exception" }
-            }
-            LogLevel.WARN -> {
-                log.warn { "Blooming Exception: ${errorType.message}, Exception: $exception" }
-            }
-            else -> {
-                log.info { "Blooming Exception: ${errorType.message}, Exception: $exception" }
-            }
-        }
+
+        logException(errorType, exception)
+
         return ResponseEntity
             .status(errorType.status)
             .body(ErrorResponse.from(errorType))
@@ -36,6 +29,8 @@ class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun handleArgumentValidationException(exception: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
         val errorType = ErrorType.BAD_REQUEST
+
+        logException(errorType, exception)
 
         // bindingResult를 순회하며 errorArgumentMap을 채운다.
         val fieldErrorList: List<FieldErrorResponse> =
@@ -58,5 +53,37 @@ class GlobalExceptionHandler {
         return ResponseEntity
             .status(exception.statusCode)
             .body(ErrorResponse.from(errorType, fieldErrorList))
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleMessageNotReadableException(exception: HttpMessageNotReadableException): ResponseEntity<ErrorResponse> {
+        val errorType = ErrorType.HTTP_MESSAGE_NOT_READABLE
+
+        logException(errorType, exception)
+
+        log.info { "Blooming Exception: ${errorType.message}, Exception: $exception" }
+
+        return ResponseEntity
+            .status(errorType.status)
+            .body(ErrorResponse.from(errorType))
+    }
+
+    private fun logException(
+        errorType: ErrorType,
+        exception: Exception,
+    ) {
+        when (errorType.logLevel) {
+            LogLevel.ERROR -> {
+                log.error { "Blooming Exception: ${errorType.message}, Exception: $exception" }
+            }
+
+            LogLevel.WARN -> {
+                log.warn { "Blooming Exception: ${errorType.message}, Exception: $exception" }
+            }
+
+            else -> {
+                log.info { "Blooming Exception: ${errorType.message}, Exception: $exception" }
+            }
+        }
     }
 }
--- common/src/main/kotlin/dnd11th/blooming/common/exception/ErrorType.kt
+++ common/src/main/kotlin/dnd11th/blooming/common/exception/ErrorType.kt
@@ -7,6 +7,7 @@ enum class ErrorType(val status: HttpStatus, var message: String, val logLevel: 
     // COMMON
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error has occurred.", LogLevel.ERROR),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "요청 파라미터가 올바르지 않습니다.", LogLevel.DEBUG),
+    HTTP_MESSAGE_NOT_READABLE(HttpStatus.BAD_REQUEST, "요청 형식이 올바르지 않습니다.", LogLevel.DEBUG),
 
     // MyPlant
     NOT_FOUND_MYPLANT(HttpStatus.NOT_FOUND, "존재하지 않는 내 식물입니다.", LogLevel.DEBUG),
