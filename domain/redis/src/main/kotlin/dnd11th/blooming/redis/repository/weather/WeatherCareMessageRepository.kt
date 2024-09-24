e6483529401d05478124fa9cf1c12b6e71b6f418
stophwan <jjw981210@gmail.com>
2024-09-25 03:11:09 +0900

    feat: Redis Repository 작성, Pipeline 사용

--- domain/redis/src/main/kotlin/dnd11th/blooming/redis/repository/weather/WeatherCareMessageRepository.kt
+++ domain/redis/src/main/kotlin/dnd11th/blooming/redis/repository/weather/WeatherCareMessageRepository.kt
@@ -1,9 +1,13 @@
 package dnd11th.blooming.redis.repository.weather
 
 import dnd11th.blooming.redis.entity.weather.WeatherCareMessage
-import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface WeatherCareMessageRepository: CrudRepository<WeatherCareMessage, String> {
+interface WeatherCareMessageRepository {
+    fun save(weatherCareMessage: WeatherCareMessage)
+
+    fun saveAll(weatherCareMessages: List<WeatherCareMessage>)
+
+    fun findByRegion(regionKey: String): WeatherCareMessage?
 }
