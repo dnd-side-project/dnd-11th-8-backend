5dfc3171493058d49f12bc786e64f787f2efef01
stophwan <jjw981210@gmail.com>
2024-10-24 02:01:07 +0900

    refactor: FCM -> Expo API로 변경

--- client/src/main/kotlin/dnd11th/blooming/client/expo/PushNotification.kt
+++ client/src/main/kotlin/dnd11th/blooming/client/expo/PushNotification.kt
@@ -1,20 +1,17 @@
-package dnd11th.blooming.client.fcm
+package dnd11th.blooming.client.expo
 
 data class PushNotification(
-    val myPlantId: Long,
-    val token: String,
+    val to: String,
     val title: String,
     val content: String,
 ) {
     companion object {
         fun create(
-            myPlantId: Long,
             token: String,
             plantNickname: String,
         ): PushNotification {
             return PushNotification(
-                myPlantId = myPlantId,
-                token = token,
+                to = token,
                 title = "블루밍",
                 content = "${plantNickname}에 물이 필요해요",
             )
